d88fb3772e0bd299a4e1f64eea92e6e5d0bac428
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-05-21 10:12:48 +0530

    Create outputs.tf

---
+++ aws/lambda/outputs.tf
@@ -0,0 +1,14 @@
+output "lambda_function_name" {
+  description = "The ID of the bucket"
+  value       = module.lambda.lambda_function_name
+}
+
+output "lambda_function_qualified_arn" {
+  description = "The ID of the bucket"
+  value       = module.lambda.lambda_function_qualified_arn
+}
+
+output "lambda_function_last_modified" {
+  description = "The ID of the bucket"
+  value       = module.lambda.lambda_function_last_modified
+}
