e9120600a6d60ed538cbf511b1cb27a40b12e2a0
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-05-21 10:12:28 +0530

    Update main.tf

--- aws/lambda/main.tf
+++ aws/lambda/main.tf
@@ -1,1 +1,10 @@
+provider "aws" {
+  # Configuration options
+  region = "us-east-2"
+}
 
+module "lambda" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "4.18.0"
+  function_name = var.function_name
+}
